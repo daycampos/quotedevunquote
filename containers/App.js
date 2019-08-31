fa73384f91d812f2a0adddb2b6154ad52bd582c7
Day Campos <dayscampos@gmail.com>
2019-08-31 11:53:49 -0300

    Adicionando emotion.js

--- containers/App.js
+++ containers/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
 export default class App extends React.Component{
+    
     render(){
+        const Title = styled.div `color: pink;`
         return(
-            <div>This is quote dev unquote</div>
+            <Title>This is quote dev unquote</Title>
         )
     }
 }
